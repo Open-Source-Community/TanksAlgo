17e64b877d9026d69919a0a68a44cf932e200549
amrtaher1234 <amrtaher1995@gmail.com>
2017-10-26 11:58:57 +0200

    cleaning some folders

--- Code Entry Part/Game/Game/AlgoTank/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.contrib.auth.models import User
-from django.db import models
-
-# Create your models here.
-class UserProfile(models.Model):
-     user =models.OneToOneField(User)
--- Code Entry Part/Game/Game/AlgoTank/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from AlgoTank import views
-
-app_name = 'AlgoTank'
-
-urlpatterns=[
- url(r'^register' ,views.regester,name=regester)
- 
-]
