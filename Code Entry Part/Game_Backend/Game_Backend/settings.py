e34e3851b9aac37d35725001bb064d47569c4581
Ahmad <ahmad.hussain.307@gmail.com>
2017-10-27 23:52:17 +0200

    SK removed

--- Code Entry Part/Game_Backend/Game_Backend/settings.py
+++ Code Entry Part/Game_Backend/Game_Backend/settings.py
@@ -19,8 +19,6 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hzz=j0hag(&+mad278cvh8tlt_09hma3elj0-=%2*xxz3iz9**'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # SECURITY WARNING: keep the secret key used in production secret!
