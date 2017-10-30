13a2d0a8918ea2926a751cae712e12c8d2f8e54c
amrtaher1234 <amrtaher1995@gmail.com>
2017-10-31 00:33:58 +0200

    a game to collect coins for the URP event

--- Tasks/TankTest/sketch.js
+++ Tasks/TankTest/sketch.js
@@ -1,44 +1,44 @@
-var sp; 
-var bool = 0; 
+var sp;
+var bool = 0;
 var posx ;
-var posy ; 
-var count =0; 
-var Tank; 
-var Tank02; 
-var pos2x=0; 
-var pos2y =0; 
-var count2 =0; 
+var posy ;
+var count =0;
+var Tank;
+var Tank02;
+var pos2x=0;
+var pos2y =0;
+var count2 =0;
 var background_color = 256;
 var counter_background=0;
 var mycolors=[] ;
 function setup() {
-  
+
   // put setup code here
- 
 
-  createCanvas(windowWidth, windowHeight); 
 
-  mycolors.push(0); 
+  createCanvas(windowWidth, windowHeight);
+
+  mycolors.push(0);
   for (var j =0; j<10; j++)
   {
-    mycolors.push(color(random(100,255) , random(120 , 255) , random(130,255) )); 
+    mycolors.push(color(random(100,255) , random(120 , 255) , random(130,255) ));
   }
-  Tank = new tank(); 
+  Tank = new tank();
   Tank.obstacles_image = "coint.png";
-  Tank.intiate(); 
- 
-  Tank02= new tank(); 
-  Tank02.intiate(); 
-  Tank02.addimage('Snail.png');
-  Tank02.sprite.mirrorY=-1; 
-  Tank02.sprite.maxSpeed=1; 
- 
-  posx = Tank.obstacles[0].position.x;
-  posy = Tank.obstacles[0].position.y; 
+  Tank.intiate();
 
-  pos2x = Tank02.obstacles[0].position.x; 
-  pos2y = Tank02.obstacles[0].position.y; 
-  
+  Tank02= new tank();
+  Tank02.intiate();
+  Tank02.addimage('Snail.png');
+  Tank02.sprite.mirrorY=-1;
+  Tank02.sprite.maxSpeed=1;
+
+  posx = Tank.obstacles[0].position.x;
+  posy = Tank.obstacles[0].position.y;
+
+  pos2x = Tank02.obstacles[0].position.x;
+  pos2y = Tank02.obstacles[0].position.y;
+
 }
 
 
@@ -47,33 +47,33 @@ function draw() {
   // put drawing code here5,
   if(Tank.sprite.overlap(Tank.obstacles[count])) // sp is the tank over here.
   {
-  
-count=count+1; 
+
+count=count+1;
 if (count%Tank.obstacles_count ==0)
-count=0; 
-posx = Tank.obstacles[count].position.x; 
-posy= Tank.obstacles[count].position.y; 
+count=0;
+posx = Tank.obstacles[count].position.x;
+posy= Tank.obstacles[count].position.y;
   }
 
 
   if(Tank02.sprite.overlap(Tank02.obstacles[count2])) // sp is the tank over here.
   {
-  
-count2=count2+1; 
-if (count2%Tank02.obstacles_count ==0)
-count2=0; 
-pos2x = Tank02.obstacles[count2].position.x; 
-pos2y= Tank02.obstacles[count2].position.y; 
+
+count2=count2+1;
+if (count2%Tank02.obstacles.length ==0)
+count2=0;
+pos2x = Tank02.obstacles[count2].position.x;
+pos2y= Tank02.obstacles[count2].position.y;
   }
- 
-  
-   Tank.attract(posx , posy); 
-   Tank02.attract(pos2x , pos2y); 
-  
-   Tank.move_obstacles(); 
-   Tank.collid_all(); 
-   Tank02.move_obstacles(); 
-  drawSprites(); 
+
+
+   Tank.attract(posx , posy);
+   Tank02.attract(pos2x , pos2y);
+
+   Tank.move_obstacles();
+   Tank.collid_all();
+   Tank02.move_obstacles();
+  drawSprites();
 
 }
 
@@ -84,26 +84,26 @@ class tank
   {
     this.sprite;
     this.image;
-    this.friction=0; 
-    this.maxspeed=3; 
-    this.scale = 0.4;  
-    this.rotateto=true; 
-    this.obstacles = new Group(); 
-    this.obstacles_count=0; 
+    this.friction=0;
+    this.maxspeed=3;
+    this.scale = 0.4;
+    this.rotateto=true;
+    this.obstacles = new Group();
+    this.obstacles_count=0;
     this.obstacles_image = "obstacle.png";
-     
+
   }
   intiate()
   {
     this.sprite = createSprite(random(width), random(height));
     //this.sprite.addImage(loadImage("Tank-GTAA.png"));
-    this.addimage("Tank-GTAA.png"); 
-    this.sprite.friction = this.friction; 
-    this.sprite.maxSpeed = this.maxspeed; 
-    this.sprite.scale = this.scale; 
-    this.sprite.rotateToDirection = this.rotateto; 
+    this.addimage("Tank-GTAA.png");
+    this.sprite.friction = this.friction;
+    this.sprite.maxSpeed = this.maxspeed;
+    this.sprite.scale = this.scale;
+    this.sprite.rotateToDirection = this.rotateto;
     this.create_obstacles();
-    
+
   }
   attract(x , y)
   {
@@ -117,29 +117,29 @@ class tank
   {
     for (var i =0; i<20; i++)
     {
-      var obstacle_temp= createSprite(random(width) , random(height) , random(30,50) , random(20,70)); 
+      var obstacle_temp= createSprite(random(width) , random(height) , random(30,50) , random(20,70));
       obstacle_temp.addImage(loadImage(this.obstacles_image));
       obstacle_temp.scale = random(0.1, 0.3);
-      this.obstacles_count++; 
+      this.obstacles_count++;
 
-      this.obstacles.add(obstacle_temp); 
-      
+      this.obstacles.add(obstacle_temp);
+
     }
   }
   move_obstacles()
   {
-    for (var i=0; i<20; i++)
+    for (var i=0; i<this.obstacles.length; i++)
     {
-      this.obstacles[i].position.x += random(-2,2); 
-      this.obstacles[i].position.y += random(-2,2); 
-      
+      this.obstacles[i].position.x += random(-2,2);
+      this.obstacles[i].position.y += random(-2,2);
+
     }
   }
   collid_all()
   {
     for (var i=0; i<20; i++)
     {
-      this.sprite.displace(this.obstacles[i]); 
+      this.sprite.displace(this.obstacles[i]);
     }
   }
 }
@@ -147,6 +147,6 @@ class tank
 function mousePressed()
 {
 
-background_color=mycolors[counter_background]; 
-counter_background= (counter_background+1)%mycolors.length; 
+background_color=mycolors[counter_background];
+counter_background= (counter_background+1)%mycolors.length;
 }
---
+++ Tasks/URP_game/sketch.js
@@ -0,0 +1,96 @@
+
+var spr;
+var attackers ;
+var player1;
+var coin_test;
+var mygame ;
+var backcolor = 50;
+function setup()
+{
+  createCanvas(windowWidth, windowHeight);
+  backcolor = color(0,191,255);
+  mygame = new game();
+  mygame.start_test();
+  mygame.populate();
+}
+
+
+
+
+
+
+function draw()
+{
+  background(backcolor);
+  mygame.view();
+  mygame.eat();
+  mygame.boom();
+  mygame.gameover();
+  mygame.goodgame();
+  mygame.showhealth();
+  mygame.move_obs();
+
+
+   drawSprites();
+}
+
+class structure{
+  constructor()
+  {
+    this.pos = createVector();
+    this.image = "Tank-GTAA.png";
+    this.speed = 2;
+    this.friction = 0.7;
+    this.sprite = null ;
+  }
+
+
+}
+class player extends structure
+{
+constructor()
+{
+  super();
+  this.score = 0;
+  this.name= "";
+
+}
+
+  attract_to(x , y)
+  {
+    this.sprite.attractionPoint(0.5 , x , y);
+  }
+  createplayer(){
+    this.sprite = createSprite(30,30 , 5 , 5);
+    this.sprite.addImage(loadImage(this.image));
+    this.sprite.scale = 0.2;
+    this.sprite.maxSpeed = this.speed;
+    this.sprite.rotateToDirection = true;
+  }
+}
+
+class coin extends structure
+{
+constructor()
+{
+  super();
+  this.image = "coint.png";
+}
+
+destroy()
+{
+  this.sprite.remove();
+  this.pos.x = -10;
+  this.pos.y = -10;
+}
+createcoin()
+{
+  this.sprite = createSprite(this.pos.x , this.pos.y);
+  this.sprite.addImage(loadImage(this.image));
+  this.sprite.scale = random(0.1 , 0.2);
+}
+set_positions(x , y){
+  this.pos.x = x;
+  this.pos.y =y;
+}
+}
