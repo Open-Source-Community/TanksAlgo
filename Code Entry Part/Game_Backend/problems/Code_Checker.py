298617e676c90661eb75b58d4909c386cc38b990
Ahmad <ahmad.hussain.307@gmail.com>
2017-11-23 00:37:44 +0200

    Added user's code testing with HR API

---
+++ Code Entry Part/Game_Backend/problems/Code_Checker.py
@@ -0,0 +1,12 @@
+#Integrating helper library for HackerRank API
+from hackerrank.HackerRankAPI import HackerRankAPI
+API_kEY = "hackerrank|1850276-1999|ba2a4949a4b24ec8a3245053cde0e3b53da869e6"
+compiler = HackerRankAPI(api_key=API_kEY)
+
+#Sends user's code to API to test against test cases
+def Check(src,test_cases,language):
+    result = compiler.run({'source': src,
+                           'lang': language, 'testcases': test_cases})
+
+    #Returns the received output from HackerRank API
+    return result.output
--- Code Entry Part/Game_Backend/problems/views.py
+++ Code Entry Part/Game_Backend/problems/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from problems.models import Problems,Test_cases,Result
+from problems.Code_Checker import Check
 
-# Renders problem statement html
+# Renders problem.html
 def Show_Problem(request):
+    #Fetches problem statement of the first problem in DB
     problem_statement = Problems.objects.only()[1].problem_statement
+    test_result = ""
 
-    # Receives user's code and calls the checker function
-    #if request.method == 'POST':
+    # Receives user's code and returns checker function result
+    if request.method == 'POST':
+        #Fetches user's choosen language
+        language = request.POST.get('lang', None)
+        #Fetches user's problem solution
+        user_solution = request.POST.get('user_solution', None)
 
-    user_solution = request.POST.get('user_solution', None)
+        # Sample test cases are used for testing
+        test_result = 'TestCase 1: ' + Check(user_solution,["1","2"],language)[0] +\
+               'TestCase 2: ' + Check(user_solution,["1","2"],language)[1]
 
     return render(request, 'problems_app/problem.html',
-                  {'problem_statement': problem_statement,'user_solution': user_solution})
+                  {'problem_statement': problem_statement,'test_result':test_result})
