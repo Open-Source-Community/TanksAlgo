5dd17226dccd7e71d531b5247cc7612a4561640b
amrtaher1234 <amrtaher1995@gmail.com>
2018-04-22 15:20:28 +0200

    deleting un-neccessary folders/files

--- Code Entry Part/Game_Backend/Registration/admin.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from Registration.models import UserProfile
-from django.contrib import admin
-
-# Register your models here.
-admin.site.register(UserProfile)
--- Code Entry Part/Game_Backend/problems/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from problems.models import Problems,Test_cases,Result
-# Register your models here.
-
-admin.site.register(Problems)
-admin.site.register(Result)
-admin.site.register(Test_cases)
